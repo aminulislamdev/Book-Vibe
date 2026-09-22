58679e9f95b3835e8364daec35effabb35c006a6
Aminul islam <aminulislam424842@gmail.com>
2026-09-22 22:40:12 +0600

    fix: resolve existing bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "./homePage/Banner";
+import Books from "./homePage/Books";
 
 export default function Home() {
   return (
     <div>
         <Banner></Banner>
+        <Books></Books>
     </div>
   );
 }
