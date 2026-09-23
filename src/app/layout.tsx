be92a2affc31cf78feab38ab15237abc577938b4
Aminul islam <aminulislam424842@gmail.com>
2026-09-23 19:52:17 +0600

    feat: setup react toastify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from './shared/Navbar.jsx'
 import Footer from "./shared/Footer";
 import BookContextProvider from "./context/BookContext";
+import { ToastContainer } from "react-toastify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           {children}
           <Footer></Footer>
         </BookContextProvider>
+
+        <ToastContainer />
       </body>
     </html>
   );
